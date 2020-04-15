5d9f58bd38908a19bc1c97d97a3a0529371ff8b7
godbass <willofgod.wb@gmail.com>
2020-04-15 03:53:12 +0000

    reponse précise 2

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(
 app.use(bodyParser.json());
 
 
+
 // scraping stats covid19
+
 function scrapData() {
     request('https://news.google.com/covid19/map?hl=fr&gl=FR&ceid=FR:fr', (error, response, html) => {
         if (!error && response.statusCode == 200) {
@@ -46,11 +48,8 @@ scrapData()
 
 
 
+// bot reception et traitement de données
 
-// Simulation
-var userInput = "Arabie saoudite";
-
-// bot
 app.post("/botcovid", function (req, res) {
 
     var demande = req.body.queryResult.parameters.demandstat;
@@ -67,17 +66,6 @@ app.post("/botcovid", function (req, res) {
         if (jsonFile[i]['pays'] == response) {
             repTab.push(jsonFile[i]['pays'], jsonFile[i]['confirmes'], jsonFile[i]['casparmillion'], jsonFile[i]['gueries'], jsonFile[i]['deces'])
         }
-    }
-    if (repTab[0]) {
-        var repPays = repTab[0];
-        var repCas = repTab[1];
-        var repCasMillion = repTab[2];
-        var repGueris = repTab[3];
-        var repDeces = repTab[4];
-        // console.log("Le pays " + repPays + " a maintenant " + repCas + " cas confirmés, " + repDeces + " décès et " + repGueris + " personnes guéries.");
-    }
-    else {
-        // console.log("Oups !! Pays non pris en compte")
     }
 
     var speechResponse = {
@@ -97,23 +85,26 @@ app.post("/botcovid", function (req, res) {
 
 
     function allstat() {
-        return response + " a maintenant " + repCas + " cas confirmes, " + repDeces + " deces et " + repGueris + " personnes gueries." + "\n" + " Soit " + repCasMillion + " car pour un million de personnes.";
+        return response + " a maintenant " + repTab[1] + " cas confirmes, " + repTab[4] + " deces et " + repTab[3] + " personnes gueries." + "\n" + " Soit " + repTab[2] + " car pour un million de personnes.";
     }
 
     function casstat() {
-        return response + " est a " + repCas + " cas confirmes, " + "\n" + " Soit " + repCasMillion + " car pour un million de personnes.";
+        return response + " est a " + repTab[1] + " cas confirmes, " + "\n" + " Soit " + repTab[2] + " car pour un million de personnes.";
     }
 
     function gueristat() {
-        return response + " a maintenant " + repGueris + " personnes gueries.";
+        return response + " a maintenant " + repTab[3] + " personnes gueries.";
     }
 
     function decesstat() {
-        return response + " a desormais " + repDeces + " deces.";
+        return response + " a desormais " + repTab[4] + " deces.";
     }
 
 
-    var result = casstat();
+    var result = gueristat();
+
+
+
 
     return res.json({
         payload: speechResponse,
